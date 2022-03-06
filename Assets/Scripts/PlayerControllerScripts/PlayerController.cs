2446ee12712c0fc8ecb60ccb9638ac5346628cf7
ZYH <72118427+ZYH771@users.noreply.github.com>
2022-03-05 22:02:15 -0500

    Added movement and attack function.

---
+++ Assets/Scripts/PlayerControllerScripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject projetile;
+    public Rigidbody2D playerRb;
+    public float playerSpeed = 5f;
+    public PlayerControls playerControls;
+    Vector2 movDirection = Vector2.zero;
+    Vector2 atkDirection = Vector2.zero;
+    private InputAction move;
+    private InputAction attack;
+    public DefaultPlayerProjectileScriptableObject defaultProjectile;
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+        attack = playerControls.Player.Shoot;
+        attack.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        attack.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movDirection = move.ReadValue<Vector2>();
+        atkDirection = attack.ReadValue<Vector2>();
+        if (atkDirection != Vector2.zero)
+        {
+            GameObject DefaultProjectile = Instantiate(projetile, transform.position, Quaternion.identity);
+            DefaultProjectile.GetComponent<Rigidbody2D>().velocity = atkDirection * defaultProjectile.speed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerRb.velocity = new Vector2(movDirection.x * playerSpeed, movDirection.y * playerSpeed);
+    }
+}
