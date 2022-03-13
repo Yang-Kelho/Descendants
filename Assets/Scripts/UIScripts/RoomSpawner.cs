7df85a66851b07f220d042991f0d3184c22c9a08
Kelho Yang <ykelho@gmail.com>
2022-03-12 21:28:40 -0500

    Prefabs, Auto Map Generation
    Added RoomPrefabs Folder
    Added AutoMapGeneration
    Added Two UI Scripts

---
+++ Assets/Scripts/UIScripts/RoomSpawner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 --> Need Bottom Door
+    // 2 --> Need Top Door
+    // 3 --> Need Right Door
+    // 4 --> Need Left Door
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 5f);
+    }
+
+    void Spawn()
+    {
+        if (spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 2)
+            {
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 3)
+            {
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if (openingDirection == 4)
+            {
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity);
+            }
+            spawned = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Spawnpoint") && other.GetComponent<RoomSpawner>().spawned == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
