738c91053f9f590bec9a8f982589e3dd63a11852
Xibin Tang <xibin.tang@yahoo.com>
2022-04-25 23:10:08 -0400

    Added notification for Login/Register

--- Assets/Scripts/UIScripts/LoginPanelCtrl.cs
+++ Assets/Scripts/UIScripts/LoginPanelCtrl.cs
@@ -9,27 +9,42 @@ public class LoginPanelCtrl : MonoBehaviour
 {
     GameObject panel_register;
     GameObject panel_start;
+    GameObject panel_notification_success;
+    GameObject panel_notification_fail;
     Button btn_login;
     Button btn_back;
     Button btn_register_transfer;
     Button btn_Logout;
+    Button btn_OK_success;
+    Button btn_OK_fail;
     App realmApp;
     private Realm _realm;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        // retrieve the panels:
+        panel_notification_success = transform.Find("panel_notification_success").gameObject;
+        panel_notification_fail = transform.Find("panel_notification_fail").gameObject;
+
         // retrieve the buttons:
         btn_login = transform.Find("btn_login").GetComponent<Button>();
         btn_back = transform.Find("btn_back").GetComponent<Button>();
         btn_Logout = transform.Find("btn_Logout").GetComponent<Button>();
         btn_register_transfer = transform.Find("btn_register_transfer").GetComponent<Button>();
+        btn_OK_fail = panel_notification_fail.transform.Find("btn_OK").GetComponent<Button>();
+        btn_OK_success = panel_notification_success.transform.Find("btn_OK").GetComponent<Button>();
+        
+
 
         // add listener and events:
         btn_login.onClick.AddListener(LoginEvent);
         btn_back.onClick.AddListener(BackEvent);
         btn_Logout.onClick.AddListener(LogoutEvent);
         btn_register_transfer.onClick.AddListener(RegisterTransferEvent);
+        btn_OK_success.onClick.AddListener(OKEvent_Success);
+        btn_OK_fail.onClick.AddListener(OKEvent_Fail);
     }
 
     private async void LoginEvent()
@@ -63,8 +78,15 @@ public class LoginPanelCtrl : MonoBehaviour
         {
             //if error catched, login failed
             Debug.Log("Login failed, invaild Credential");
+            // pop out the "login fail" panel
+            panel_notification_fail.SetActive(true);
         }
 
+        // pop out the "login success" panel
+        if(panel_notification_fail.activeSelf == false)
+        {
+            panel_notification_success.SetActive(true);
+        }
     }
 
     private async void LogoutEvent()
@@ -100,6 +122,15 @@ public class LoginPanelCtrl : MonoBehaviour
             await realmApp.CurrentUser.LogOutAsync();
         }
         _realm.Dispose();
+    }
+
+    private void OKEvent_Success()
+    {
+        panel_notification_success.SetActive(false);
+    }
+    private void OKEvent_Fail()
+    {
+        panel_notification_fail.SetActive(false);
     }
 
 }
--- Assets/Scripts/UIScripts/RegisterPanelCtrl.cs
+++ Assets/Scripts/UIScripts/RegisterPanelCtrl.cs
@@ -8,19 +8,34 @@ using Realms.Sync;
 public class RegisterPanelCtrl : MonoBehaviour
 {
     GameObject panel_login;
+    GameObject panel_notification_success;
+    GameObject panel_notification_fail;
     Button btn_register;
     Button btn_back;
+    Button btn_OK_success;
+    Button btn_OK_fail;
     private Realm _realm;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        // retrieve the panels:
+        panel_notification_success = transform.Find("panel_notification_success").gameObject;
+        panel_notification_fail = transform.Find("panel_notification_fail").gameObject;
+
+        // retrieve the buttons:
         btn_register = transform.Find("btn_register").GetComponent<Button>();
         btn_back = transform.Find("btn_back").GetComponent<Button>();
+        btn_OK_fail = panel_notification_fail.transform.Find("btn_OK").GetComponent<Button>();
+        btn_OK_success = panel_notification_success.transform.Find("btn_OK").GetComponent<Button>();
+
 
         //add listener and events:
         btn_register.onClick.AddListener(RegisterEvent);
         btn_back.onClick.AddListener(BackEvent);
+        btn_OK_success.onClick.AddListener(OKEvent_Success);
+        btn_OK_fail.onClick.AddListener(OKEvent_Fail);
 
     }
 
@@ -53,13 +68,14 @@ public class RegisterPanelCtrl : MonoBehaviour
             //if return value = 0. Register failed
             await realmApp.CurrentUser.LogOutAsync();
             _realm.Dispose();
+            panel_notification_fail.SetActive(true);
             Debug.Log("Reigster failed, duplicated id exists");
         }
         else
         {
             await realmApp.CurrentUser.LogOutAsync();
             _realm.Dispose();
-            BackEvent();
+            panel_notification_success.SetActive(true);
         }
     }
 
@@ -68,5 +84,15 @@ public class RegisterPanelCtrl : MonoBehaviour
         panel_login = transform.parent.Find("panel_login").gameObject;
         panel_login.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    private void OKEvent_Success()
+    {
+        panel_notification_success.SetActive(false);
+        BackEvent();
+    }
+    private void OKEvent_Fail()
+    {
+        panel_notification_fail.SetActive(false);
     }
 }
