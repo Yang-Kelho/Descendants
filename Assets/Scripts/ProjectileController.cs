7bffa2274e079a0ea3b8c210bd44c3b662bcb7fd
Kelho Yang <ykelho@gmail.com>
2022-04-27 12:19:27 -0400

    RoomPF & UI Tweaks
    Walls now properly destroy projectiles;
    UI is slightly anchored to screen;

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -30,5 +30,9 @@ public class ProjectileController : MonoBehaviour
                 }
             }
         }
+        if (collision.CompareTag("Walls"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
